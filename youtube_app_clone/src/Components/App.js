acc9e01a52a4919832969887138b9611ce612e3f
fausto08 <faustol99@gmail.com>
2019-07-21 19:16:24 -0400

    added event handlers
    added the prevent default function (onFormSubmit), and inputChange.

--- youtube_app_clone/src/Components/App.js
+++ youtube_app_clone/src/Components/App.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import SearchBar from './SearchBar';
+import React from "react";
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   render() {
     return (
-    
-      <div>
+      <div className="ui container">
         <SearchBar />
-        
-        </div>
-    
+      </div>
     );
-
   }
 }
 
